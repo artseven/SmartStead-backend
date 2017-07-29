2e219380fd04f05c8a92533a4ce413a6e4bdf109
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-29 08:29:45 -0400

    push latest

--- routes/camera-api-routes.js
+++ routes/camera-api-routes.js
@@ -1,23 +1,10 @@
 const express = require('express');
 const ensure = require('connect-ensure-login');
-
+const http = require('http');
 const router = express.Router();
 const path = require('path');
 const request = require('request');
 
-
-// var options = {
-//     method: 'GET',
-//     url: 'http://usa22543.mytenvis.org/cgi-bin/hi3510/ptzctrl.cgi',
-//     headers: { authorization: 'Basic YWRtaW46dGVudmlzMUA=' },
-//     qs: { '-step': '1', '-act': 'left', '-speed': '45', 'username': 'admin', 'password':  }
-// };
-
-// request(options, function(error, response, body) {
-//     if (error) throw new Error(error);
-
-//     console.log(body);
-// });
 
 router.post('/api/camera/left', (req, res, next) => {
     var options = {
@@ -27,20 +14,6 @@ router.post('/api/camera/left', (req, res, next) => {
         headers: { authorization: 'Basic YWRtaW46dGVudmlzMUA=' }
     };
 
-    request(options, function(error, response, body) {
-        if (error) throw new Error(error);
-
-        console.log(body);
-    });
-});
-
-router.post('/api/camera/right', (req, res, next) => {
-    var options = {
-        method: 'GET',
-        url: 'http://usa22543.mytenvis.org/cgi-bin/hi3510/ptzctrl.cgi',
-        qs: { '-step': '1', '-act': 'right', '-speed': '45' },
-        headers: { authorization: 'Basic YWRtaW46dGVudmlzMUA=' }
-    };
 
     request(options, function(error, response, body) {
         if (error) throw new Error(error);
@@ -49,34 +22,49 @@ router.post('/api/camera/right', (req, res, next) => {
     });
 });
 
-router.post('/api/camera/down', (req, res, next) => {
-    var options = {
-        method: 'GET',
-        url: 'http://usa22543.mytenvis.org/cgi-bin/hi3510/ptzctrl.cgi',
-        qs: { '-step': '1', '-act': 'down', '-speed': '45' },
-        headers: { authorization: 'Basic YWRtaW46dGVudmlzMUA=' }
-    };
+// router.post('/api/camera/right', (req, res, next) => {
+//     var options = {
+//         method: 'GET',
+//         url: 'http://usa22543.mytenvis.org/cgi-bin/hi3510/ptzctrl.cgi',
+//         qs: { '-step': '1', '-act': 'right', '-speed': '45' },
+//         headers: { authorization: 'Basic YWRtaW46dGVudmlzMUA=' }
+//     };
 
-    request(options, function(error, response, body) {
-        if (error) throw new Error(error);
+//     request(options, function(error, response, body) {
+//         if (error) throw new Error(error);
 
-        console.log(body);
-    });
-});
+//         console.log(body);
+//     });
+// });
 
-router.post('/api/camera/up', (req, res, next) => {
-    var options = {
-        method: 'GET',
-        url: 'http://usa22543.mytenvis.org/cgi-bin/hi3510/ptzctrl.cgi',
-        qs: { '-step': '1', '-act': 'up', '-speed': '45' },
-        headers: { authorization: 'Basic YWRtaW46dGVudmlzMUA=' }
-    };
+// router.post('/api/camera/down', (req, res, next) => {
+//     var options = {
+//         method: 'GET',
+//         url: 'http://usa22543.mytenvis.org/cgi-bin/hi3510/ptzctrl.cgi',
+//         qs: { '-step': '1', '-act': 'down', '-speed': '45' },
+//         headers: { authorization: 'Basic YWRtaW46dGVudmlzMUA=' }
+//     };
 
-    request(options, function(error, response, body) {
-        if (error) throw new Error(error);
+//     request(options, function(error, response, body) {
+//         if (error) throw new Error(error);
 
-        console.log(body);
-    });
-});
+//         console.log(body);
+//     });
+// });
+
+// router.post('/api/camera/up', (req, res, next) => {
+//     var options = {
+//         method: 'GET',
+//         url: 'http://usa22543.mytenvis.org/cgi-bin/hi3510/ptzctrl.cgi',
+//         qs: { '-step': '1', '-act': 'up', '-speed': '45' },
+//         headers: { authorization: 'Basic YWRtaW46dGVudmlzMUA=' }
+//     };
+
+//     request(options, function(error, response, body) {
+//         if (error) throw new Error(error);
+
+//         console.log(body);
+//     });
+// });
 
 module.exports = router;
