237ebc56efb9456a4e3b9f734b37e8fff520d0a5
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-28 21:11:23 -0400

    redoing the routes

--- routes/light-api-routes.js
+++ routes/light-api-routes.js
@@ -8,36 +8,66 @@ const ShoppingItem = require('../models/shopping-list.js');
 const UserModel = require('../models/user-model.js')
 const request = require('request');
 
-// router.get('api/lights', (req, res, next) => {
-//     res => res.json();
+// Set the headers
+var headers = {
+    'User-Agent': 'Super Agent/0.0.1',
+    'Content-Type': 'application/x-www-form-urlencoded'
+}
+
+// Configure the request
+var options = {
+    url: 'https://www.meethue.com/api/sendmessage?token=8ee7d03f19ec6cc3b9ed11229e5689b1a59b34f1a4575fad8127d5a0b5f77014',
+    method: 'POST',
+    headers: headers,
+    form: {
+        clipmessage = {
+            bridgeId: "001788fffe4c72e9",
+            clipCommand: {
+                url: "/api/0/groups/0/action",
+                method: "PUT",
+                body: { "on": true, "bri": 200 }
+            }
+        }
+    }
+}
+
+// Start the request
+request(options, function(error, response, body) {
+        if (!error && response.statusCode == 200) {
+            // Print out the response body
+            console.log(body)
+        }
+    })
+    // router.get('api/lights', (req, res, next) => {
+    //     res => res.json();
 
 // });
 
-router.post('api/lights', (req, res, next) => {
+// router.post('api/lights', (req, res, next) => {
 
-        // set options
-        const options = {
-            url: `https://www.meethue.com/api/sendmessage?token=8ee7d03f19ec6cc3b9ed11229e5689b1a59b34f1a4575fad8127d5a0b5f77014`,
-            body: clipmessage = {
-                bridgeId: "001788fffe4c72e9",
-                clipCommand: {
-                    url: "/api/0/groups/0/action",
-                    method: "PUT",
-                    body: { "on": false, "bri": 200 }
-                }
-            }
-        }),
-    dataType: 'json'
-    // headers: {
-    //     // 'Authorization': `Bearer ${req.body.accessToken}`,
-    //     'Content-Type': 'application/json'
-    // }
-};
+//         // set options
+//         const options = {
+//             url: `https://www.meethue.com/api/sendmessage?token=8ee7d03f19ec6cc3b9ed11229e5689b1a59b34f1a4575fad8127d5a0b5f77014`,
+//             body: clipmessage = {
+//                 bridgeId: "001788fffe4c72e9",
+//                 clipCommand: {
+//                     url: "/api/0/groups/0/action",
+//                     method: "PUT",
+//                     body: { "on": false, "bri": 200 }
+//                 }
+//             }
+//         }),
+//     dataType: 'json'
+//     // headers: {
+//     //     // 'Authorization': `Bearer ${req.body.accessToken}`,
+//     //     'Content-Type': 'application/json'
+//     // }
+// };
 
-// post request
-request.post(options, (err, response, body) => {
-    // send status to spotify service
-    res.status(200).json(body);
-});
+// // post request
+// request.post(options, (err, response, body) => {
+//     // send status to spotify service
+//     res.status(200).json(body);
+// });
 
 module.exports = router;
