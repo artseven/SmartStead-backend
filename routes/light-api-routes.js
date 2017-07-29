129572242929504e4035487ae8b6f51d6d068808
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-07-29 00:30:12 -0400

    adding urlencoded package

--- routes/light-api-routes.js
+++ routes/light-api-routes.js
@@ -8,7 +8,8 @@ const https = require('https');
 const ShoppingItem = require('../models/shopping-list.js');
 const UserModel = require('../models/user-model.js')
 const request = require('request');
-
+const querystring = require('querystring')
+const urlencode = require('urlencode');
 
 
 
@@ -16,31 +17,65 @@ router.post('/api/lights/on', (req, res, next) => {
     console.log("MY POST REQUEST STARTS HERE-----")
         // Set the headers
     var headers = {
-            'Content-Type': 'application/x-www-form-urlencoded'
+        'Content-Type': 'application/x-www-form-urlencoded',
+        // 'Content-Length': postBody.length
+    }
+
+    //the POST request's body data
+    var object = {
+        bridgeId: "001788fffe4c72e9",
+        clipCommand: {
+            url: "/api/0/groups/0/action",
+            method: "PUT",
+            body: {
+                "on": true,
+                "bri": 200
+            }
         }
-        // options
+    };
+
+    var encodedObject = urlencode(object, { charset: "UTF-8" });
+    console.log("Encoded" + encodedObject);
+
+    // postBody = querystring.stringify(postData);
+    urlencode(object, { charset: 'UTF-8' });
+    console.log("ENCODED" + urlencode(object, { charset: 'UTF-8' }));
+
+    // options
     var options = {
         url: 'https://www.meethue.com/api/sendmessage?token=8ee7d03f19ec6cc3b9ed11229e5689b1a59b34f1a4575fad8127d5a0b5f77014',
         method: 'POST',
         headers: headers,
         form: {
-            clipmessage: {
-                bridgeId: "001788fffe4c72e9",
-                clipCommand: {
-                    url: "/api/0/groups/0/action",
-                    method: "PUT",
-                    body: {
-                        "on": true,
-                        "bri": 200
-                    }
-                }
-            }
+            clipmessage: object
+                // clipmessage: {
+                //     bridgeId: "001788fffe4c72e9",
+                //     clipCommand: {
+                //         url: "/api/0/groups/0/action",
+                //         method: "PUT",
+                //         body: {
+                //             "on": true,
+                //             "bri": 200
+                //         }
+                //     }
+                // }
         }
 
     }
 
-    // Start the request
-    request.post(options, function(error, response, body) {
+    // var postreq = https.request(options, function(res) {
+    //     //Handle the response
+    //     // console.log('BODY before' + body);
+    //     if (!error && response.statusCode == 200) {
+    //         // Print out the response body
+    //         console.log("Response body" + body)
+    //     }
+    // });
+    // postreq.write(postBody);
+    // postreq.end();
+
+    //Start the request
+    request(options, function(error, response, body) {
         // console.log('BODY before' + body);
         if (!error && response.statusCode == 200) {
             // Print out the response body
@@ -50,33 +85,33 @@ router.post('/api/lights/on', (req, res, next) => {
 });
 
 
-router.post('/api/lights/off', (req, res, next) => {
+// router.post('/api/lights/off', (req, res, next) => {
 
-    // options
-    // var options = {
-    //     url: 'https://www.meethue.com/api/sendmessage?token=8ee7d03f19ec6cc3b9ed11229e5689b1a59b34f1a4575fad8127d5a0b5f77014',
-    //     method: 'POST',
-    //     headers: headers,
-    //     form: {
-    //         "clipmessage": {
-    //             bridgeId: "001788fffe4c72e9",
-    //             clipCommand: {
-    //                 url: "/api/0/groups/0/action",
-    //                 method: "PUT",
-    //                 body: { "on": false }
-    //             }
-    //         }
-    //     }
-    // }
+//     // options
+//     // var options = {
+//     //     url: 'https://www.meethue.com/api/sendmessage?token=8ee7d03f19ec6cc3b9ed11229e5689b1a59b34f1a4575fad8127d5a0b5f77014',
+//     //     method: 'POST',
+//     //     headers: headers,
+//     //     form: {
+//     //         "clipmessage": {
+//     //             bridgeId: "001788fffe4c72e9",
+//     //             clipCommand: {
+//     //                 url: "/api/0/groups/0/action",
+//     //                 method: "PUT",
+//     //                 body: { "on": false }
+//     //             }
+//     //         }
+//     //     }
+//     // }
 
-    // Start the request
-    request(options, function(error, response, body) {
-        if (!error && response.statusCode == 200) {
-            // Print out the response body
-            console.log(body)
-        }
-    });
-});
+//     // Start the request
+//     request(options, function(error, response, body) {
+//         if (!error && response.statusCode == 200) {
+//             // Print out the response body
+//             console.log(body)
+//         }
+//     });
+// });
 //                 bridgeId: "001788fffe4c72e9",
 //                 clipCommand: {
 //                     url: "/api/0/groups/0/action",
