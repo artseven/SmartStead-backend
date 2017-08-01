c3232fdf08d51757407d98a38366e5ec3add2e6c
Art Yabkevich <yabkevich.artsiom.09@gmail.com>
2017-08-01 10:21:21 -0400

    adding routing

--- routes/nest-api-routes.js
+++ routes/nest-api-routes.js
@@ -4,62 +4,50 @@ const router = express.Router();
 
 require('dotenv').config();
 
-// GET AUTHORIZATION TOKEN
-var options = {
-    method: 'POST',
-    url: 'https://api.home.nest.com/oauth2/access_token',
-    headers: { 'content-type': 'application/x-www-form-urlencoded' },
-    form: {
-        client_id: (process.env.CLIENT_ID),
-        client_secret: (process.env.CLIENT_SECRET),
-        grant_type: (process.env.GRANT_TYPE),
-        code: (process.env.CODE)
-    }
-};
 
-request(options, function(error, response, body) {
-    if (error) throw new Error(error);
+router.post('/api/nest/stats', (req, res, next) => {
+    // GET JSON WITH STATS
+    var options = {
+        method: 'GET',
+        url: 'https://developer-api.nest.com/',
+        headers: {
+            authorization: '(process.env.NEST_TOKEN)',
+            'content-type': 'application/json'
+        },
+        form: {
+            client_id: (process.env.CLIENT_ID),
+            client_secret: (process.env.CLIENT_SECRET),
+            grant_type: (process.env.GRANT_TYPE),
+            code: (process.env.CODE)
+        }
+    };
 
-    console.log(body);
-});
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
 
-// GET JSON WITH STATS
-var options = {
-    method: 'GET',
-    url: 'https://developer-api.nest.com/',
-    headers: {
-        authorization: 'Bearer ' + (process.env.NEST_TOKEN),
-        'content-type': 'application/json'
-    },
-    form: {
-        client_id: (process.env.CLIENT_ID),
-        client_secret: (process.env.CLIENT_SECRET),
-        grant_type: (process.env.GRANT_TYPE),
-        code: (process.env.CODE)
-    }
-};
-
-request(options, function(error, response, body) {
-    if (error) throw new Error(error);
-
-    console.log(body);
+        console.log(body);
+    });
 });
 
 
-//POST TEMPERATURE 75
-var options = {
-    method: 'PUT',
-    url: 'https://developer-api.nest.com/devices/thermostats/fBPMKukMCSry2ORduyQXdPh71Io6C2rc',
-    headers: {
-        authorization: '(process.env.NEST_TOKEN)',
-        'content-type': 'application/json'
-    },
-    body: { target_temperature_f: 72 },
-    json: true
-};
 
-request(options, function(error, response, body) {
-    if (error) throw new Error(error);
 
-    console.log(body);
+router.post('/api/nest/temperature', (req, res, next) => {
+    //POST TEMPERATURE 75
+    var options = {
+        method: 'PUT',
+        url: 'https://developer-api.nest.com/devices/thermostats/fBPMKukMCSry2ORduyQXdPh71Io6C2rc',
+        headers: {
+            authorization: '(process.env.NEST_TOKEN)',
+            'content-type': 'application/json'
+        },
+        body: { target_temperature_f: 72 },
+        json: true
+    };
+
+    request(options, function(error, response, body) {
+        if (error) throw new Error(error);
+
+        console.log(body);
+    });
 });
